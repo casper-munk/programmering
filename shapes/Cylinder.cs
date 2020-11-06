be8b6dd8647dc8cb56e5c3a8c8cd76814d8be57f
Casper <casper>
2020-11-06 14:08:14 +0100

    cylinder klar

--- shapes/Cylinder.cs
+++ shapes/Cylinder.cs
@@ -5,6 +5,26 @@ namespace Interface1
 {
     class Cylinder
     {
+
+            protected double radius;
+             protected double height;
+           
+        public Cylinder(double radius1, double height1)
+        {
+            radius = radius1;
+            height = height1;
+          
+
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius * height;
+        }
+        public double GetVolume()
+        {
+            return 2 * Math.PI * radius * height;
+        }
         
     }
 }
--- shapes/Databas.cs
+++ shapes/Databas.cs
@@ -12,6 +12,10 @@ namespace Interface1
         databas = new List<Shape>();
         Sphere spheres = new Sphere(3);
         databas.Add(spheres);
+        Cube cubes = new Cube(4);
+        databas.Add(cubes);
+        Cylinder cylinders = new Cylinder(3);
+        databas.Add(cylinders);
     }
 
 }
